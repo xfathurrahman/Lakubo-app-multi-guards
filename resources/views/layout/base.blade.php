ca50c45bea40c23d6a4c243ed348c5715cc52078
xfathurrahman <fathurrahmanx018@gmail.com>
2022-11-11 22:49:41 +0700

    tambah homepage

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -18,9 +18,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Rubick Admin Template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="LEFT4CODE">
-
     @yield('head')
-
     <!-- BEGIN: CSS Assets-->
     @vite('resources/css/app.css')
     <!-- END: CSS Assets-->
