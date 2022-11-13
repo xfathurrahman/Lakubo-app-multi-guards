aaf11862e955853c316a51e7a4e4eb12198555e0
xfathurrahman <fathurrahmanx018@gmail.com>
2022-11-14 02:26:10 +0700

    tambah page register

--- resources/views/layout/auth/register/index.blade.php
+++ resources/views/layout/auth/register/index.blade.php
@@ -28,24 +28,54 @@
                     <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">Mendaftar</h2>
                     <div class="intro-x mt-2 text-slate-400 dark:text-slate-400 xl:hidden text-center">Beberapa langkah untuk Mendaftar. Lakubo - Lapak UMKM Boyolali</div>
                     <div class="intro-x mt-8">
-                        <input type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="First Name">
-                        <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Last Name">
+                        <input type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Masukan Nama Lengkap">
+                        <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Nomor Hp">
                         <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Email">
+                        <!-- BEGIN: Nested Select Kecamatan -->
+                        <div class="mt-3">
+                            <div class="mt-2">
+                                <select data-placeholder="Cari Kecamatan" class="tom-select w-full">
+                                    <optgroup label="Kecamatan">
+                                        <option> --Pilih Kecamatan-- </option>
+                                        @foreach($provinces as $provinsi)
+                                            <option value="{{$provinsi->id}}">{{ $provinsi->name }}</option>
+                                        @endforeach
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- END: Nested Select Kecamatan -->
+                        <!-- BEGIN: Nested Select Desa -->
+                        <div class="mt-3">
+                            <div class="mt-2">
+                                <select data-placeholder="Cari Desa" class="tom-select w-full">
+                                    <optgroup label="Desa">
+                                        <option value="1">--Pilih Desa--</option>
+                                        <option value="2">Winong</option>
+                                        <option value="3">Jelok</option>
+                                        <option value="4">Kenteng</option>
+                                        <option value="5">Mojosongo</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- END: Nested Select -->
+                        <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Alamat Detail (RT/RW/Nomor Rumah)">
                         <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Kata Sandi">
-                        <div class="intro-x w-full grid grid-cols-12 gap-4 h-1 mt-3">
+                        {{--<div class="intro-x w-full grid grid-cols-12 gap-4 h-1 mt-3">
                             <div class="col-span-3 h-full rounded bg-success"></div>
                             <div class="col-span-3 h-full rounded bg-success"></div>
                             <div class="col-span-3 h-full rounded bg-success"></div>
                             <div class="col-span-3 h-full rounded bg-slate-100 dark:bg-darkmode-800"></div>
                         </div>
-                        <a href="" class="intro-x text-slate-500 block mt-2 text-xs sm:text-sm">Apa itu sandi yang aman?</a>
+                        <a href="" class="intro-x text-slate-500 block mt-2 text-xs sm:text-sm">Apa itu sandi yang aman?</a>--}}
                         <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Konfirmasi sandi">
                     </div>
-                    <div class="intro-x flex items-center text-slate-600 dark:text-slate-500 mt-4 text-xs sm:text-sm">
+                    {{--<div class="intro-x flex items-center text-slate-600 dark:text-slate-500 mt-4 text-xs sm:text-sm">
                         <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
                         <label class="cursor-pointer select-none" for="remember-me">Saya setuju dengan</label>
                         <a class="text-primary dark:text-slate-200 ml-1" href="">Privacy Policy</a>.
-                    </div>
+                    </div>--}}
                     <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                         <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Mendaftar</button>
                         <a href="{{route('login.index')}}">
@@ -58,3 +88,45 @@
         </div>
     </div>
 @endsection
+
+{{--
+@section('script')
+    <script>
+        $(function (){
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        });
+
+
+        $(function (){
+            $(function (){
+
+                $('#provinsi').on('change',function (){
+                    let id_provinsi = $('#provinsi').val()
+
+                    $.ajax({
+                        type : 'POST',
+                        url : "{{route('getkabupaten')}}",
+                        data : {id_provinsi:id_provinsi},
+                        cache : false,
+
+                        success:: function (msg){
+                            $('#kabupaten').html(msg);
+                            $('#kecamatan').('');
+                            $('#desa').('');
+                        },
+                        error: function (data){
+                            console.log('error:',data)
+                        },
+                    })
+
+                })
+
+            })
+        });
+
+    </script>
+@endsection--}}
